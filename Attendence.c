aa741aaeffe002c7551a394103af2b7493006079
Athul-soman <athulsoman0829@gmail.com>
2026-07-17 11:22:48 +0530

    Update Attendence.c

--- Attendence.c
+++ Attendence.c
@@ -13,6 +13,17 @@ int main() {
     printf("Enter your minimum percentage: ");
     scanf("%d",&Min);
 //error check
+if (Min == 100 && P != T)
+     {
+         printf("\nYou will not get 100 percentage at this  attendence");
+         return 1;
+         
+     }
+     else if(Min >100)
+     {
+         printf("\npercentage must be less than or equalto 100");
+         return 1;
+     }
     if(T<P){
         printf("Total hours must be greater than attented hours\n Try again \n");
     
